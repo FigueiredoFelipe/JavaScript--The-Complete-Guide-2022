d195ee122042baed75263ed297e02983a4a8b3b2
FigueiredoFelipe <111877813+FigueiredoFelipe@users.noreply.github.com>
2023-02-12 18:33:38 -0300

    fix: folder location

--- 08-arrays-01-starting-code/sets-maps.js
+++ 08-arrays-01-starting-code/sets-maps.js
@@ -12,10 +12,25 @@
 //     console.log(entry);
 // }
 
-const person1 = {name: 'Max'}
-const person2 = {name: 'Manuel'}
+const person1 = { name: 'Max' }
+const person2 = { name: 'Manuel' }
 
-const personData = new Map([[person1, [{date: 'yesterday', price: 10}]]])
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]])
 
-// console.log(personData);
+personData.set(person2, [{date: 'two weeks ago', price: 100}])
+
+console.log(personData);
 console.log(personData.get(person1));
+console.log(personData.get(person2));
+
+for (const [key, value] of personData.entries()){
+    console.log(key, value);
+}
+
+for (const key of personData.keys()){
+    console.log(key);
+    console.log(key);
+    console.log(key);
+}
+
+
